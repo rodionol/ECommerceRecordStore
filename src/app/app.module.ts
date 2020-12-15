9a2f077faff35915779ca37c412aab1800aa2757
parminder036 <75805660+parminder036@users.noreply.github.com>
2020-12-15 18:01:55 -0500

    home and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import * as Highcharts from 'highcharts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 import { AddProductComponent } from './add-product/add-product.component';
 import { SearchProductComponent } from './search-product/search-product.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HighchartsChartComponent } from 'highcharts-angular';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     AddProductComponent,
     SearchProductComponent,
+    HighchartsChartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
+  highcharts =  Highcharts;
+  chartOptions = {   
+     chart : {
+        plotBorderWidth: null,
+        plotShadow: false,
+        backgroundColor:null
+     },
+     title : {
+        text: 'Available Products' ,
+        style: {
+           color: '#FFFFFF'
+        }
+          
+     },
+     plotOptions : {
+        pie: {
+           allowPointSelect: true,
+           cursor: 'pointer',
+           colors: [
+              '#000000',
+              '#FFFFFF'
+           ],
+           dataLabels: {
+              enabled: false       
+           },
+     
+           showInLegend: true
+        }
+     },
+     series : [{
+        type: 'pie',
+        name: 'share',
+        
+        data: [
+           ['availableProducts',25],
+           ['unusedProducts', 75],
+                 ]
+     }]
+  };
+  
+  }
 
-}
