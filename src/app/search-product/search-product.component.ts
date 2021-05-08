f0702c9f482db33760fc91f73b45803233607069
rodionol <lrodionova@gmail.com>
2021-05-07 23:54:46 -0400

    a11y fixes throughout site

--- src/app/search-product/search-product.component.ts
+++ src/app/search-product/search-product.component.ts
@@ -61,12 +61,6 @@ export class SearchProductComponent implements OnInit {
   filter(category : string){
     console.log('category: ', category);
     this.foundProducts = []; // clear old search data
-    // this.productService.getProductByGenre(category).subscribe((data:Product) => {  
-    //   console.log('data:', data);
-    //   this.foundProducts.push(data);
-    //   // console.log('DATA:', data);
-    //   console.log(this.foundProducts, '(foundProducts array)');  
-    // });
     PRODUCTS.forEach(product => {
       if (product.genre.toLowerCase() == category.toLowerCase()) {
         this.foundProducts.push(product);
